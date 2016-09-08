abf38ada8d3efd6cd95d78c2e7628e5e5d199850
juvekeeper41 <juvekeeper41@gmail.com>
2016-09-08 09:27:19 -0600

    Edited the controller

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -1,18 +1,31 @@
 package stupid.controller;
 
+import java.util.Scanner;
+
 public class StupidController 
 {
 	private String myName;
+	private Scanner inputReader;
 	
 	public  StupidController()
 	{
 		myName = "Jacob Osborne";
+		inputReader = new Scanner(System.in);
 	}
 	
 	public void start()
 	{
 		System.out.println("This program is less stupid.");
 		System.out.println("My name is " + myName);
+		askQuestions();
+	}
+
+
+private void askQuestions()
+	{
+		System.out.println("How is your day?");
+		String input = inputReader.nextLine();
+		System.out.println("Oh, you said: " + input + " about your day.");
 	}
 }
 
