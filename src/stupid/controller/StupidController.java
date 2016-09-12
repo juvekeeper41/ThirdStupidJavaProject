9c4f62cdedbfbf6fa44b0ff0cc52d13ee9d23b33
juvekeeper41 <juvekeeper41@gmail.com>
2016-09-12 09:49:00 -0600

    Created a series of questions

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -26,6 +26,38 @@ private void askQuestions()
 		System.out.println("How is your day?");
 		String input = inputReader.nextLine();
 		System.out.println("Oh, you said: " + input + " about your day.");
+		
+		System.out.println("My next question is this: What is the best type of food?");
+		String foodInput = inputReader.nextLine();
+		System.out.println("OK, you typed this, right?: " + foodInput);
+	    inputReader.nextLine(); 
+		
+		System.out.println("How much do you weigh?");
+		int input1 = inputReader.nextInt();	
+		System.out.println("You said that you weigh: " + input1 + " pounds.");
+		inputReader.nextLine(); 		
+		
+		System.out.println("How old are you?");
+		int input2 = inputReader.nextInt();
+		System.out.println("You claim to be " + input2 + " years old!");
+		inputReader.nextLine(); 		
+		
+		System.out.println("What are the first ten digits of pi?");
+		inputReader.nextDouble();
+		System.out.println("3.141592653 is the correct answer, yes?");
+		inputReader.nextLine();
+		
+		System.out.println("What is the answer to 2+2?");
+		int someInput = inputReader.nextInt();
+		System.out.println("OK, you typed this: " + someInput + ". The correct answer is 4. Type anything to continue the questions.");
+		inputReader.nextLine(); 		
+		String other = inputReader.next();
+		System.out.println(other);
+		
+		System.out.println("Do my questions bother you?");
+		System.out.println("I'm sorry, if so. I was programmed to ask them.");
+		
 	}
+
 }
 
